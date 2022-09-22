0d9ab4e474a85178eb272a56b21225954747afa6
websiterunner <samfuller3478@gmail.com>
2022-09-22 07:06:48 +0530

    Fix: background;

---
+++ _next/static/GuVmjPQtrEQyu1yLg4hEb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-6cab716966a244cb.js","static/chunks/750-e2f248dc72c4b46e.js","static/chunks/pages/index-e0812fa2d2542866.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/yz5DVMLumUS-cdDf-Go-v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-f8750f6ee0af8022.js","static/chunks/801-90a5a5dbab9dac4c.js","static/chunks/pages/index-021bf16a663cda11.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
